99d5170eeb31d06c0d19713d692f97f3ed8ede08
Enmanuel Cruz <enmanuelcruzdejesus3@gmail.com>
2026-02-16 14:14:07 -0300

    restoring navbar

--- ankor-training/vite.config.ts
+++ ankor-training/vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Allow access via ngrok or other external hosts.
-    allowedHosts: true,
-  },
 })
